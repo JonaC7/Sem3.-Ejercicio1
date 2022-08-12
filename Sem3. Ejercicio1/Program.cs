b2614f4e483b6a8754e8d8641358cee90fa1dbde
Alumnos <Alumnos@LABC2PC18>
2022-08-12 09:05:05 -0600

    Agregar archivos de proyecto.

---
+++ Sem3. Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sem3._Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
