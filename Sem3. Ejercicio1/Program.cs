84fbe68e7bee0b13025b4e3dbc2b1b443ecad391
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 09:22:06 -0600

    ejercicio 1

--- Sem3. Ejercicio1/Program.cs
+++ Sem3. Ejercicio1/Program.cs
@@ -6,7 +6,25 @@ namespace Sem3._Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese su nota promedio");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota>=0 && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else if (nota >= 5)
+                {
+                    Console.WriteLine("Reposicion");
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
         }
+
     }
 }
