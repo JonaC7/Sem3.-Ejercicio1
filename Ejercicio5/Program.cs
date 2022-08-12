54e26cfb93466f9881ea3429bbcfa7d1e8c0b79d
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 10:20:54 -0600

    Ejercicio 5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.WriteLine("Ingrese un valor entre 0 y 99 (0 finaliza);:");
+                linea = Console.ReadLine();
+                valor = int.Parse
+            }
+        }
+
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
