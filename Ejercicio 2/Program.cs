7138200e89c2c5a8c208cf3ff8cbab740ea26eae
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 09:38:09 -0600

    Ejercicio 2

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Elijió la opción {0}", option);
+                    break;
+                case 2:
+                    Console.WriteLine("Elijió la opción {0}", option);
+                    break;
+                case 3:
+                    Console.WriteLine("Elijió la opción {0}", option);
+                    break;
+                case 4:
+                    Console.WriteLine("Elijió la opción {0}", option);
+                    break;
+                case 5:
+                    Console.WriteLine("Elijió la opción {0}", option);
+                    break;
+                default:
+                    Console.WriteLine("Esta opción no está disponible");
+                    break;
+            }
+        }
+    }
+}
